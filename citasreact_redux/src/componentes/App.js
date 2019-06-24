2ef2228845bc7aa89ce10b24b2e770a55e123ae2
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-06-24 07:25:54 -0500

    Reducers, Store, Componentes, Dependencias:redux react-redux redux-thunk

--- citasreact_redux/src/componentes/App.js
+++ citasreact_redux/src/componentes/App.js
@@ -4,6 +4,10 @@ import AgregarCita from './AgregarCita';
 import ListaCitas from './ListaCitas';
 import Imagen from './Imagen';
 
+// ***** Redux ***** 
+import { Provider } from 'react-redux';
+import store from './../store/store';
+
 class App extends Component {
 
   state = {
@@ -81,39 +85,41 @@ class App extends Component {
 
   render(){
     return(
-      <div className="App container">
-        {/* ***** Componente: Header ***** */}
-        <Header
-                titulo={'Administrador de Pacientes de Veterinaria'}
-        ></Header>
-        {/* ***** Componente: Agregar Cita ***** */}
-        <div className="row">
-          <div className="col-md-6">
-            <AgregarCita
-                          crearCita={this.crearCita}
-            ></AgregarCita>
+      <Provider store={store}>
+        <div className="App container">
+          {/* ***** Componente: Header ***** */}
+          <Header
+                  titulo={'Administrador de Pacientes de Veterinaria'}
+          ></Header>
+          {/* ***** Componente: Agregar Cita ***** */}
+          <div className="row">
+            <div className="col-md-6">
+              <AgregarCita
+                            crearCita={this.crearCita}
+              ></AgregarCita>
+            </div>
+            {/* ***** Componente: ListaCitas ***** */}
+            <div className="col-md-6">
+              <ListaCitas
+                          citas={this.state.citas}
+                          borrarCita={this.borrarCita}
+              ></ListaCitas>
+            </div>
           </div>
-          {/* ***** Componente: ListaCitas ***** */}
-          <div className="col-md-6">
-            <ListaCitas
-                        citas={this.state.citas}
-                        borrarCita={this.borrarCita}
-            ></ListaCitas>
+          {/* ***** Componente: Imagen ***** */}
+          <div className="row">
+            <div className="col-md-4 centro">
+              <Imagen></Imagen>
+            </div>
+            <div className="col-md-4 centro">
+              <h1 className='tamaño'>React</h1>
+            </div>
+            <div className="col-md-4 centro">
+              <Imagen></Imagen>
+            </div>
           </div>
         </div>
-        {/* ***** Componente: Imagen ***** */}
-        <div className="row">
-          <div className="col-md-4 centro">
-            <Imagen></Imagen>
-          </div>
-          <div className="col-md-4 centro">
-            <h1 className='tamaño'>React</h1>
-          </div>
-          <div className="col-md-4 centro">
-            <Imagen></Imagen>
-          </div>
-        </div>
-      </div>
+      </Provider>
     )
   }
 }
---
+++ citasreact_redux/src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+// Importar los reducers
+import reducerPrincipal from './../reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(reducerPrincipal, initialState, compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
